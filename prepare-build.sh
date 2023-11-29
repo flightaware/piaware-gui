5053ab8873feeaea2395ae18a2d4d1bc644e2cb4
eric1tran <eric1tran@gmail.com>
2023-11-29 21:36:17 +0000

    Update backport version for  bullseye builds

--- prepare-build.sh
+++ prepare-build.sh
@@ -50,6 +50,8 @@ case "$DIST" in
         ;;
 
     bullseye)
+        echo "Updating changelog for bullseye backport build" >&2
+        dch --changelog $OUT/debian/changelog --local ~bpo11+ --force-distribution --distribution bullseye-backports "Automated backport build for bullseye">
         ;;
 
     *)
